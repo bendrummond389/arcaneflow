7d00e546c5749cb90e23ec5f03a85e693ff04d2a
bendrummond389 <60265150+bendrummond389@users.noreply.github.com>
2025-03-02 11:11:40 -0600

    Update transformation.py

--- src/arcaneflow/data/transformation.py
+++ src/arcaneflow/data/transformation.py
@@ -3,6 +3,7 @@ from typing import Optional
 import pandas as pd
 
 from arcaneflow.data.data_frame_schema import DataFrameSchema
+from arcaneflow.data.transformation_chain import TransformationChain
 
 class BaseTransformation(ABC):
     input_schema: Optional[DataFrameSchema]
@@ -17,10 +18,9 @@ class BaseTransformation(ABC):
         if self.output_schema:
             self.output_schema.validate(transformed)
 
-
-
-
     @abstractmethod
     def _transform(self, df: pd.DataFrame) -> pd.DataFrame:
         pass
 
+    def chain(self, next_transformation: 'BaseTransformation') -> 'TransformationChain':
+        return TransformationChain([self, next_transformation])
